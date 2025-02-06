32297128719f8537beeb53e4707b80c3c6fd5118
djdjown <71399200+djdjown@users.noreply.github.com>
2025-02-06 11:30:38 +0300

    Update Main.java

--- cezar/src/Main.java
+++ cezar/src/Main.java
@@ -1,16 +1,11 @@
 public class Main {
 
-    // Метод для шифрования текста с использованием сдвига
     public static String encrypt(String text, int shift) {
         StringBuilder encryptedText = new StringBuilder();
-
         for (int i = 0; i < text.length(); i++) {
             char c = text.charAt(i);
-
-            // Шифруем только буквы
             if (Character.isLetter(c)) {
                 char base = (Character.isLowerCase(c)) ? 'a' : 'A';
-                // Сдвигаем букву в пределах алфавита
                 c = (char) ((c - base + shift) % 26 + base);
             }
 
@@ -20,9 +15,8 @@ public class Main {
         return encryptedText.toString();
     }
 
-    // Метод для дешифрования текста с использованием сдвига
     public static String decrypt(String text, int shift) {
-        return encrypt(text, 26 - shift); // Дешифрование = шифрование с обратным сдвигом
+        return encrypt(text, 26 - shift); 
     }
 
     public static void main(String[] args) {
